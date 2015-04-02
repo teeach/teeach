65fdb5fc77fd9ab05836dd99c1a09af09446ca3c
polectron <polectron@gmail.com>
2015-04-03 00:58:57 +0200

    Añadir cambio de contraseña

--- pl_panel/usr/editprofile.php
+++ pl_panel/usr/editprofile.php
@@ -1,7 +1,7 @@
 <?php
 	include('../../core.php'); 
-	//~ include('../../usr.php');
-
+	include('../../PasswordHash.php');
+    
     session_start();
     $h = $_SESSION['h'];
 
@@ -10,18 +10,41 @@
     $User = $System->get_user_by_id($h, $connection);
 
     if ($_GET['action']=="success") {
+        $error = 0;
 		$name = $_POST['name'];
 		$subname1 = $_POST['subname1'];
 		$subname2 = $_POST['subname2'];
 		$email = $_POST['email'];
 		$url_img = $_POST['url_img'];
-
-		$query = $connection->query("update pl_users set name='$name' where id=$User->id")or die("Error!");
-		$query = $connection->query("update pl_users set subname1='$subname1' where id=$User->id")or die("Error!");
-		$query = $connection->query("update pl_users set subname2='$subname2' where id=$User->id")or die("Error!");
-		$query = $connection->query("update pl_users set email='$email' where id=$User->id")or die("Error!");
-		$query = $connection->query("update pl_users set photo='$url_img' where id=$User->id")or die("Error!");
-		header("Location: profile.php?h=".$User->h);
+        if($_POST['act_password'] != $_POST['new_password']){
+            
+            $t_hasher = new PasswordHash(8, FALSE);
+            
+            $query = $connection->query("select * from pl_users where username='$User->username'") or die ("Error!");
+            
+            $row = mysqli_fetch_array($query);
+            
+            $pass_db = $row['pass'];
+            
+            $check = $t_hasher->CheckPassword($_POST['act_password'], $pass_db);
+            
+            if ($check) {
+                $t_hasher = new PasswordHash(8, FALSE);
+                $pass_hash = $t_hasher->HashPassword($_POST['new_password']);
+                $query = $connection->query("update pl_users set pass='$pass_hash' where id=$User->id")or die("Error!");
+            }else{
+                echo 'Contraseña incorrecta';
+                $error += 1;
+            }
+        }
+        if($error == 0){
+            $query = $connection->query("update pl_users set name='$name' where id=$User->id")or die("Error!");
+            $query = $connection->query("update pl_users set subname1='$subname1' where id=$User->id")or die("Error!");
+            $query = $connection->query("update pl_users set subname2='$subname2' where id=$User->id")or die("Error!");
+            $query = $connection->query("update pl_users set email='$email' where id=$User->id")or die("Error!");
+            $query = $connection->query("update pl_users set photo='$url_img' where id=$User->id")or die("Error!");
+            //~ header("Location: profile.php?h=".$User->h);
+        }
 	}
     
 ?>
@@ -67,9 +90,9 @@
 			<table>
 			<form method="post" action="editprofile.php?action=success" name="perfil" style="padding: 10px">
 				<tr><td><label for="name">'._("Name: ").'</label></td><td><input type="text" name="name" value="'.$User->name.'"></td></tr>
-				<tr><td>'._("Subnames: ").'</td><td><input type="text" name="subname1" value="'.$User->surname1.'">, <input type="text" name="subname2" value="'.$User->surname2.'"></td></tr>
+				<tr><td>'._("Surnames: ").'</td><td><input type="text" name="subname1" value="'.$User->surname1.'">, <input type="text" name="subname2" value="'.$User->surname2.'"></td></tr>
 				<tr><td><label for="email">'._("Email: ").'</label></td><td><input type="text" name="email" value="'.$User->email.'"></td></tr>			
-				<tr><td>'._("Password: ").'</td><td><a href="#">Cambiar contraseña</a></td></tr>
+				<tr><td>'._("Change password <br>(Not changed if left blank): ").'</td><td>Actual password: <input type="password" name="act_password"></td><td>New password: <input type="password" name="new_password"></td></tr>
 				<tr><td>'._("Profile photo: ").'</td><td><input id="photo_url" type="text" value="'.$User->photo.'" name="url_img"></td></tr>
 				<tr><td><img id="previsualizar_img"></td><td></td></tr>
 				<tr><td><input type="submit" value="Enviar"></td><td></td></tr>
@@ -78,9 +101,7 @@
 		';
 
 	$System->set_footer(); ?>
-<!--
-	<script>comprobarImagen();</script>
--->
+
     <script>
         $("#photo_url" ).keyup(function() {
             var value = $( this ).val();
