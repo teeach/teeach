58a6a3c2d9d4d7bb584093e1678d6dfe923b9955
polectron <polectron@gmail.com>
2015-04-02 23:39:13 +0200

    Improve image loader

--- pl_panel/usr/editprofile.php
+++ pl_panel/usr/editprofile.php
@@ -1,10 +1,8 @@
 <?php
 	include('../../core.php'); 
-	include('../../usr.php');
+	//~ include('../../usr.php');
 
-	$System = new System();
-
-	if ($_GET['action']=="success") {
+    if ($_GET['action']=="success") {
 
 		$name = $_POST['name'];
 		$subname1 = $_POST['subname1'];
@@ -12,15 +10,20 @@
 		$email = $_POST['email'];
 		$url_img = $_POST['url_img'];
 
-		$System->conDB("../../config.json");
-
-		$query = $con->query("update pl_users set name='$name' where id=$usr_id")or die("Error!");
+		$query = $con->query("update pl_users set name='$name' where id=$User->id")or die("Error!");
 		$query = $con->query("update pl_users set subname1='$subname1' where id=$usr_id")or die("Error!");
 		$query = $con->query("update pl_users set subname2='$subname2' where id=$usr_id")or die("Error!");
 		$query = $con->query("update pl_users set email='$email' where id=$usr_id")or die("Error!");
 		$query = $con->query("update pl_users set photo='$url_img' where id=$usr_id")or die("Error!");
 		header("Location: profile.php?h=".$usr_h);
 	}
+
+    session_start();
+    $h = $_SESSION['h'];
+
+    $System = new System();
+    $connection = $System->conDB("../../config.json");
+    $User = $System->get_user_by_id($h, $connection);
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +36,7 @@
 		$System = new System();
 		$System->set_head(); 
 	?>
+<!--
 	<script>
 		function comprobarImagen() {
 			var url_img = document.perfil.url_img.value;
@@ -46,12 +50,13 @@
 				}
 			}
 			if(!permitida) {
-				alert("Project Learn sólo admite las extensíones gif, jpg, png y swg.");
+				alert("Project Learn sólo admite las extensiones gif, jpg, png y swg.");
 			} else {
 				document.getElementById("previsualizar_img").src = url_img;
 			}
 		}
 	</script>
+-->
 </head>
 <body>
 	<?php 
@@ -61,18 +66,27 @@
 		echo '
 			<table>
 			<form method="post" action="editprofile.php?action=success" name="perfil" style="padding: 10px">
-				<tr><td><label for="name">'._("Name: ").'</label></td><td><input type="text" name="name" value="'.$usr_name.'"></td></tr>
-				<tr><td>'._("Subnames: ").'</td><td><input type="text" name="subname1" value="'.$usr_subname1.'">, <input type="text" name="subname2" value="'.$usr_subname2.'"></td></tr>
-				<tr><td><label for="email">'._("Email: ").'</label></td><td><input type="text" name="email" value="'.$usr_email.'"></td></tr>			
+				<tr><td><label for="name">'._("Name: ").'</label></td><td><input type="text" name="name" value="'.$User->name.'"></td></tr>
+				<tr><td>'._("Subnames: ").'</td><td><input type="text" name="subname1" value="'.$User->surname1.'">, <input type="text" name="subname2" value="'.$User->surname2.'"></td></tr>
+				<tr><td><label for="email">'._("Email: ").'</label></td><td><input type="text" name="email" value="'.$User->email.'"></td></tr>			
 				<tr><td>'._("Password: ").'</td><td><a href="#">Cambiar contraseña</a></td></tr>
-				<tr><td>'._("Profile photo: ").'</td><td><input type="text" value="'.$usr_photo.'" name="url_img" onblur="comprobarImagen()"></td></tr>
-				<tr><td><img id="previsualizar_img"></td><td></td></tr>
+				<tr><td>'._("Profile photo: ").'</td><td><input id="photo_url" type="text" value="'.$User->photo.'" name="url_img"></td></tr>
+				<tr><td><img src="" id="previsualizar_img"></td><td></td></tr>
 				<tr><td><input type="submit" value="Enviar"></td><td></td></tr>
 			</form>
 			</table>
 		';
 
 	$System->set_footer(); ?>
+<!--
 	<script>comprobarImagen();</script>
+-->
+    <script>
+        $( "#photo_url" ).keyup(function() {
+            var value = $( this ).val();
+            $( "#previsualizar_img" ).attr("src", value);
+        })
+        .keyup();
+    </script>
 </body>
 </html>
