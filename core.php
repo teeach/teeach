52194b26b0c778fda1b02dc8fc093831ce5cf309
polectron <polectron@gmail.com>
2015-05-17 14:52:36 +0200

    Añadir funcion rand_string()

--- core.php
+++ core.php
@@ -103,6 +103,17 @@
             return $user;
         }
         
+        function rand_string( $length ) {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";	
+
+            $size = strlen( $chars );
+            for( $i = 0; $i < $length; $i++ ) {
+                $str .= $chars[ rand( 0, $size - 1 ) ];
+            }
+
+            return $str;
+        }
+        
 	}
     
     class User{
